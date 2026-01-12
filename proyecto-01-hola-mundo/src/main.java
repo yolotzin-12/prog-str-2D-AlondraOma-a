67b7601c72b8e44f37445b7c5f5fcf7c821d041a
Stev220707 <20253ds155@utez.edu.mx>
2026-01-12 09:02:44 -0600

    Creacion del main del poryecto hola mundo

---
+++ proyecto-01-hola-mundo/src/main.java
@@ -0,0 +1,7 @@
+public class main{
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Alondra Yolotzin Omaña Silva 2-D 12/01/2026");
+
+    }
+}
