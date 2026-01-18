f22e142d55bd8d7f951bd71bcfbb0d38b325ccc3
yolotzin-12 <20253ds154@utez.edu.mx>
2026-01-17 19:06:41 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/Main.java
+++ MergeWithOutConflict/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
         System.out.println("Semana 2");
         System.out.println("Versión base");
+        System.out.println("2 + 2 = 4");
+        System.out.println("5 * 3 = 15");
 
     }
 
